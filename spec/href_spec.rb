1d92d213ebb97d4b6f80a968063f1ed187683926
Mike Slinn <mslinn@micronauticsresearch.com>
2022-12-18 13:16:46 -0500

    -

--- spec/href_spec.rb
+++ spec/href_spec.rb
@@ -36,7 +36,7 @@ class TestParseContext < Liquid::ParseContext
 end
 
 # Lets get this party started
-class MyTest
+class MyTest # rubocop:disable Metrics/ClassLength
   RSpec.describe ExternalHref do
     let(:logger) do
       PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
@@ -52,7 +52,7 @@ class MyTest
       )
     end
 
-    it "Obtains external link without text" do
+    it "Obtains external link with text" do
       href = ExternalHref.send(
         :new,
         'href',
@@ -62,7 +62,106 @@ class MyTest
       href.send(:globals_initial, parse_context)
       linkk = href.send(:compute_linkk)
       href.send(:globals_update, href.helper.argv, linkk)
-      expect(href.link).to eq('https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss')
+      expect(href.follow).to eq(" rel='nofollow'")
+      expect(href.link).to   eq('https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss')
+      expect(href.target).to eq(" target='_blank'")
+      expect(href.text).to   eq('SoundCloud RSS Feed')
+    end
+
+    it "Obtains external link without scheme or text" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq(" rel='nofollow'")
+      expect(href.link).to   eq('https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss')
+      expect(href.target).to eq(" target='_blank'")
+      expect(href.text).to   eq('<code>feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss</code>')
+    end
+
+    it "Obtains external link with follow" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'follow https://www.mslinn.com Awesome'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq('')
+      expect(href.link).to   eq('https://www.mslinn.com')
+      expect(href.target).to eq(" target='_blank'")
+      expect(href.text).to   eq('Awesome')
+    end
+
+    it "Obtains external link with follow and notarget" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'follow notarget https://www.mslinn.com Awesome'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq('')
+      expect(href.link).to   eq('https://www.mslinn.com')
+      expect(href.target).to eq('')
+      expect(href.text).to   eq('Awesome')
+    end
+
+    it "Obtains external link with follow and notarget but without text" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'follow notarget www.mslinn.com'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq('')
+      expect(href.link).to   eq('https://www.mslinn.com')
+      expect(href.target).to eq('')
+      expect(href.text).to   eq('<code>www.mslinn.com</code>')
+    end
+
+    it "Obtains mailto without text" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'mailto:mslinn@mslinn.com'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq('')
+      expect(href.link).to   eq('mailto:mslinn@mslinn.com')
+      expect(href.target).to eq('')
+      expect(href.text).to   eq('<code>mslinn@mslinn.com</code>')
+    end
+
+    it "Obtains mailto with text" do
+      href = ExternalHref.send(
+        :new,
+        'href',
+        'mailto:mslinn@mslinn.com Mike Slinn'.dup,
+        parse_context
+      )
+      href.send(:globals_initial, parse_context)
+      linkk = href.send(:compute_linkk)
+      href.send(:globals_update, href.helper.argv, linkk)
+      expect(href.follow).to eq('')
+      expect(href.link).to   eq('mailto:mslinn@mslinn.com')
+      expect(href.target).to eq('')
+      expect(href.text).to   eq('Mike Slinn')
     end
   end
 end
