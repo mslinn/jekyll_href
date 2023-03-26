aa40d2ef6b3b50cfed5ed9e56b637371232787b4
Mike Slinn <mslinn@mslinn.com>
2023-03-26 11:11:06 -0400

    -

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,14 +1,14 @@
-hrefs = {}
+@hrefs = {}
 
 def add_link_to_page(page_path, link)
-  hrefs = [] if hrefs.nil?
-  hrefs[page_path] << link
+  @hrefs[page_path] = [] if @hrefs[page_path].nil?
+  @hrefs[page_path] << link
 end
 
-add_link_to_page'page1', 'link1'
-add_link_to_page'page1', 'link2'
+add_link_to_page 'page1', 'link1'
+add_link_to_page 'page1', 'link2'
 
-add_link_to_page'page2', 'link3'
-add_link_to_page'page2', 'link4'
+add_link_to_page 'page2', 'link3'
+add_link_to_page 'page2', 'link4'
 
-puts hrefs
+puts @hrefs
