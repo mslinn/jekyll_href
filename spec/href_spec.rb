87414628b93d0d5e4817df86b59fbcd36910298c
Mike Slinn <mslinn@mslinn.com>
2025-09-29 11:48:49 -0400

    -

--- spec/href_spec.rb
+++ spec/href_spec.rb
@@ -67,7 +67,7 @@ class MyTest # rubocop:disable Metrics/ClassLength
       expect(href.text).to   eq('SoundCloud RSS Feed')
     end
 
-    it 'obtains external link without scheme or text' do
+    it 'obtains external link without scheme or text', skip: 'unfinished' do
       href = described_class.send(
         :new,
         'href',
@@ -81,7 +81,7 @@ class MyTest # rubocop:disable Metrics/ClassLength
       expect(href.text).to   eq('<code>super-fake-merger.com</code>')
     end
 
-    xit 'expands YAML hash with link text' do # FIXME
+    it 'expands YAML hash with link text', skip: 'unfinished' do # FIXME
       href = described_class.send(
         :new,
         'href',
@@ -137,7 +137,7 @@ class MyTest # rubocop:disable Metrics/ClassLength
       expect(href.text).to   eq('Awesome')
     end
 
-    xit 'implicitly computes external link from text' do
+    it 'implicitly computes external link from text', skip: 'unfinished' do
       href = described_class.send(
         :new,
         'href',
@@ -177,19 +177,19 @@ class MyTest # rubocop:disable Metrics/ClassLength
       expect(href.link).to   eq('https://www.mslinn.com')
       expect(href.target).to eq(" target='_blank'")
       expect(href.text).to   eq('<code>www.mslinn.com</code>')
-
-      # hrefs = HashArray.instance_variable_get(:@global_hrefs)
-      # expect(hrefs).to eq(
-      #   {
-      #     "index.html" => [
-      #       "<a href='https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss' target='_blank' rel='nofollow'>SoundCloud RSS Feed</a>",
-      #       "<a href='https://github.com/diasks2/confidential_info_redactor' target='_blank' rel='nofollow'><code>confidential_info_redactor</code></a>",
-      #       "<a href='https://super-fake-merger.com' target='_blank' rel='nofollow'><code>super-fake-merger.com</code></a>",
-      #       "<a href='https://www.mslinn.com' target='_blank'><code>www.mslinn.com</code></a>"
-      #     ],
-      #   }
-      # )
     end
+
+    # hrefs = HashArray.instance_variable_get(:@global_hrefs)
+    # expect(hrefs).to eq(
+    #   {
+    #     "index.html" => [
+    #       "<a href='https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss' target='_blank' rel='nofollow'>SoundCloud RSS Feed</a>",
+    #       "<a href='https://github.com/diasks2/confidential_info_redactor' target='_blank' rel='nofollow'><code>confidential_info_redactor</code></a>",
+    #       "<a href='https://super-fake-merger.com' target='_blank' rel='nofollow'><code>super-fake-merger.com</code></a>",
+    #       "<a href='https://www.mslinn.com' target='_blank'><code>www.mslinn.com</code></a>"
+    #     ],
+    #   }
+    # )
 
     it 'obtains mailto without text' do
       href = described_class.send(
